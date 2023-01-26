dc545f144387ec207e80de57540fb9f9a87a2335
xelos97 <rakshit876@icloud.com>
2023-01-26 08:10:00 +0530

    Update app.js
    Optimised code

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 function digitalTime() {
     var dateTime = new Date();
     var h = dateTime.getHours();
-    var m = dateTime.getMinutes();
-    var s = dateTime.getSeconds();
+    var m = dateTime.getMinutes().toString().padStart(2, '0');
+    var s = dateTime.getSeconds().toString().padStart(2, '0');
 
-    var amPm = document.getElementById("am-pm")
-
-    if (h >= 12) {
-        amPm.innerHTML = "PM";
-    } else {
-        amPm.innerHTML = "AM";
-    }
-
-    if (h > 12) {
-        h = h - 12
-    }
-
-    document.getElementById("hours").innerHTML = h;
-    document.getElementById("minutes").innerHTML = m;
-    document.getElementById("seconds").innerHTML = s;
+    h = h > 12 ? h - 12 : h;
+    h = h.toString().padStart(2, '0');
+    var amPm = h >= 12 ? "PM" : "AM";
 }
 
-setInterval(digitalTime, 50);
+var amPmEl = document.getElementById("am-pm");
+var hoursEl = document.getElementById("hours");
+
+setInterval(function(){
+    digitalTime();
+    hoursEl.innerHTML = `${h}:${m}:${s} ${amPm}`;
+}, 50);
